d91858e6f8789f86b20d737fd770bbf07dbbaecf
Kitt Cheung <kitt.cheung@se.experis.com>
2022-01-18 10:26:45 +0100

    kata solution

---
+++ app.js
@@ -0,0 +1,29 @@
+
+function getLength(input){
+    let count = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (Array.isArray(input[i])) {
+            count+= getLength(input[i])
+        }else{ count++;}
+        
+    }
+    return count;
+}
+console.log(getLength([1, [2, 3]]));
+
+
+
+
+function getLength2(input){
+    let count = 0;
+    for (let element of input) {
+        if (Array.isArray(element)) {
+            count+= getLength(element)
+        }else{
+    
+            count++;
+        }
+    }
+    return count;
+}
+console.log(getLength2([1, [2, [3, [4, [5, 6]]]]]));
